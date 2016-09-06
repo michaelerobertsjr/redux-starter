532fd0bd8eabd2a440bb994eaa19951640eaa540
Michael Roberts Jr <michael.r@zeetogroup.com>
2016-09-06 15:02:22 -0700

    updates to use connect without decorators and removes thunk

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,22 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import UserList from "./UserList";
-
 import { fetchUsers } from "../Actions/userActions";
-
-//@connect((store) => {
-//	return {
-//		users: store.users.users,
-//	}
-//})
 
 const App = React.createClass({
 	componentWillMount(){
 		this.props.dispatch(fetchUsers());
 	},
-
 	render(){
-		console.log(`USERS ::: ${this.props.users}`);
 		return (
 			<div>
 			App Component
